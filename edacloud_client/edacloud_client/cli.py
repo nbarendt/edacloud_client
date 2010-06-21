0e414daeceaca479509b7981d72478fa2e1a567a
nbarendt@nickmacbookpro.local <nbarendt@nickmacbookpro.local>
2010-06-21 13:05:17 -0400

    adding simple web test support

--- edacloud_client/edacloud_client/cli.py
+++ edacloud_client/edacloud_client/cli.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python 
 from cmd import Cmd
+from urllib2 import urlopen
 
 class EDACloudClient(Cmd):
     prompt = 'edacloud> '
+    server_hostname = 'localhost'
+    server_portnumber = 8080
+
+    def __init__(self, completekey='tab', stdin=None, stdout=None):
+        Cmd.__init__(self, completekey, stdin, stdout)
+
     def do_echo(self, args):
         self.stdout.write(args)
 
@@ -13,6 +20,9 @@ class EDACloudClient(Cmd):
     def do_EOF(self, args):
         return self.do_quit(args)
 
+    def do_datetime(self, args):
+        result = urlopen('http://{0}:{1}/datetime'.format(self.server_hostname, self.server_portnumber))
+        self.stdout.write(result.read())
 
 
 if __name__ == '__main__':
--- edacloud_client/tests/acceptance/test_cmd.py
+++ edacloud_client/tests/acceptance/test_cmd.py
@@ -4,6 +4,8 @@ from time import sleep
 import os
 from StringIO import StringIO
 from edacloud_client.cli import EDACloudClient
+import BaseHTTPServer
+from threading import Thread
 
 python_exe = 'python'
 
@@ -14,27 +16,62 @@ ECHO_CMD = 'echo'
 QUIT_CMD = 'quit'
 
 ECHO_LINE = 'Hello!'
-TEST_ECHO_LINE = '{0} {1}'.format(ECHO_CMD, ECHO_LINE)
+TEST_ECHO_CMDLINE = '{0} {1}'.format(ECHO_CMD, ECHO_LINE)
+EXPECTED_DATETIME_ISO_STRING = '2010-06-21T10:49:49.230427'
 
 class PopenCLITestCase(TestCase):
     def test_WillEcho(self):
-
         cmdline = [python_exe, os.path.join(MODULE_NAME, CLI_NAME)]
         proc = Popen(cmdline, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-        stdoutdata, stderrdata = proc.communicate('\n'.join([TEST_ECHO_LINE , QUIT_CMD]))
+        stdoutdata, stderrdata = proc.communicate('\n'.join([TEST_ECHO_CMDLINE , QUIT_CMD]))
         self.assertIn(ECHO_LINE, stdoutdata, 'cmd.py failed to echo')
+
 
 class CLITestCase(TestCase):
     def setUp(self):
         self.stdout_buffer = StringIO()
         self.cmd = EDACloudClient(stdout=self.stdout_buffer)
+        self.httpd_server = None
 
     def tearDown(self):
         self.cmd = None
         self.stdout_buffer.close()
         self.stdout_buffer = None
+        self.stop_http_test_server()
+
+    def start_http_test_server(self, req_handler):
+        self.server_hostname = 'localhost'
+        self.server_portnumber = 0
+        self.httpd_server = BaseHTTPServer.HTTPServer( (self.server_hostname, self.server_portnumber),
+                                                        req_handler)
+        self.cmd.server_hostname = self.httpd_server.server_address[0]
+        self.cmd.server_portnumber = self.httpd_server.server_address[1]
+        self.server_thread = Thread(target=self.httpd_server.serve_forever)
+        self.server_thread.daemon = True
+        self.server_thread.start()        
+
+    def stop_http_test_server(self):
+        if self.httpd_server:
+            self.httpd_server.shutdown()
+            self.server_thread.join()
+            self.server_thread = None
+            self.httpd_server = None
 
     def test_WillEcho(self):
-        self.cmd.onecmd(TEST_ECHO_LINE)
+        self.cmd.onecmd(TEST_ECHO_CMDLINE)
         self.assertIn(ECHO_LINE, self.stdout_buffer.getvalue())
+
+    def test_WillGetTimeFromServer(self):
+        GET_TIME_CMDLINE = 'datetime'
+        class GetDateTimeTestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+            def do_GET(self):
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(EXPECTED_DATETIME_ISO_STRING)
+                return
+        self.start_http_test_server(GetDateTimeTestHandler)
+        self.cmd.onecmd(GET_TIME_CMDLINE)
+        self.assertEquals(EXPECTED_DATETIME_ISO_STRING, self.stdout_buffer.getvalue())
+
+
                          
